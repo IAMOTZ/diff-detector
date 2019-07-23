3f8b80a8758029f1303567b6bd9987fa9479c6ed
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2019-07-23 11:43:13 +0100

    Add codes to implement diff detector

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 /**
- * This is the main entrypoint to your Probot app
+ * This is the main entrypoint to your app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+module.exports = (app) => {
+  const pathsToCheck = (process.env.PATH_TO_CHECK || '').trim().split(',');
+  const pathsToCheckRegExp = pathsToCheck.map(filePath => new RegExp(`diff --git a/(.+?${filePath}.+?) b/(.+?${filePath}.+)`, 'g'));
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.log('App configured to check the following paths: ', pathsToCheck);
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('pull_request.opened', async (context) => {
+    context.log('Pull request event handler triggered.');
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    const { diff_url } = context.payload.pull_request;
+
+    context.log('Fetching pull request diffs from: ', diff_url);
+    const { data: diffs } = await context.github.request({ url: diff_url });
+    context.log('These are the file diffs I got: ', diffs);
+
+    context.log('Identifying expected file diffs...');
+    let fileDiffs = [];
+    pathsToCheckRegExp.forEach((regExp) => {
+      const match = diffs.match(regExp);
+      if (match) {
+        fileDiffs = [...fileDiffs, ...match];
+      }
+    });
+
+    context.log('Expected file diffs: ', fileDiffs);
+
+    if (fileDiffs.length) {
+      context.log('Identifying expected changed files');
+      const fileChanges = fileDiffs.map(file => file.match(new RegExp('diff --git a/(.+?) '))[1]);
+      context.log('These are the file changes:\n', fileChanges.join('\n'));
+
+      const message = ''
+        + 'Hi there!\n'
+        + 'Please ensure that the file changes in the following components have the required story documents:'
+        + '\n```\n'
+        + fileChanges.join('\n')
+        + '\n```\n'
+        + 'Thanks.';
+      context.log('Creating a comment on the PR...');
+      const issueComment = context.issue({ body: message });
+      return context.github.issues.createComment(issueComment);
+    }
+    context.log('There are no file changes... Event handler is done executing.');
+  });
+};
